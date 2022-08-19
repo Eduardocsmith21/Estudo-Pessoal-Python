da86130c00244396e04d54a5e2654155d1cbc207
Eduardo Carvalho Smith <111188049+Eduardocsmith21@users.noreply.github.com>
2022-08-19 20:32:16 -0300

    Update NumerosPrimos.py

--- NumerosPrimos.py
+++ NumerosPrimos.py
@@ -1,4 +1,4 @@
-num = int(input('Digite um número: '))
+num = int(input("Digite um número: "))
 tot = 0
 for c in range(1,num + 1):
     if num % c == 0:
@@ -7,12 +7,12 @@ for c in range(1,num + 1):
     else:
         print('', end=' ')
     print('{}'.format(c),end=' ')
-print('O número {} '.format(num))
+print("O número {} ".format(num))
 print("É divísiveil {} vezes".format(num,tot))
 if tot == 2:
-    print('E por isso ele é PRIMO!')
+    print("E por isso ele é PRIMO!")
 else:
-    print('E por isso ele NÃO É PRIMO!')
+    print("E por isso ele NÃO É PRIMO!")
 
 
 
