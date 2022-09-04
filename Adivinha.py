1b08b30cddf756abc030fa2bfb6fa6228aefda0c
Eduardo Carvalho Smith <eduardocsmith021@gmail.com>
2022-09-03 21:19:09 -0300

    Create Adivinha.py

---
+++ Adivinha.py
@@ -0,0 +1,17 @@
+
+from random import randint
+
+from time import sleep
+
+Computador = randint(0, 10)
+print('Vou pensa em um numero entre 0 e 10 tenta adivinha....')
+jogador = int(input('Em que numero eu pensei: '))
+print('PROCESSANDO...')
+sleep(1)
+if jogador == Computador:
+    print('PARABENS VOCÊ CONSEGUIU ME VENCER!')
+else:
+    print('GANHEI! pensei no numero {} e não '
+          ' {}'.format(Computador,jogador))
+
+
