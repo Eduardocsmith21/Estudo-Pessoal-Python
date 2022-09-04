615cb879f261741afc37acb7468dede6944d1aa8
Eduardo Carvalho Smith <111188049+Eduardocsmith21@users.noreply.github.com>
2022-09-03 21:20:18 -0300

    Update Adivinha.py

--- Adivinha.py
+++ Adivinha.py
@@ -4,7 +4,7 @@ from random import randint
 from time import sleep
 
 Computador = randint(0, 10)
-print('Vou pensa em um numero entre 0 e 10 tenta adivinha....')
+print('Vou pensa em um numero entre 0 e 10 tenta adivinhar....')
 jogador = int(input('Em que numero eu pensei: '))
 print('PROCESSANDO...')
 sleep(1)
